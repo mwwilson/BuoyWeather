962099d9d9117fb25097f16ef5b3581c380283bd
Wilson, Marshall <WilsonMa@advisory.com>
2018-08-22 20:02:53 -0500

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Simple Weather App</h1>
+
+          <div className="content">
+          </div>
+        </div>
+    );
+  }
+}
+ 
+export default Main;
